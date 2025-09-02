a0f63194e69674e739287ca6d54d97ffc45ab69e
x-annaa <annaannamx@gmail.com>
2025-09-01 23:03:05 -0700

    Update home.js

--- frontend/home.js
+++ frontend/home.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const pages = document.querySelectorAll(".page");
-  const buttons = document.querySelectorAll(".bottom-nav button");
 
-  buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      // 隐藏所有页面 & 取消按钮高亮
-      pages.forEach(page => page.classList.remove("active"));
-      buttons.forEach(btn => btn.classList.remove("active"));
-
-      // 显示目标页面 & 高亮当前按钮
-      document.getElementById(button.dataset.page).classList.add("active");
-      button.classList.add("active");
-    });
-  });
-});
